37c5d37ab0a06e61e90693668ae644b949366799
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-06-11 01:12:25 +0600

    feat: add python version info

--- python/index.py
+++ python/index.py
@@ -1,5 +1,8 @@
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
 
 print('Py!')
 print('--- Returned 10 month later :) ---')
-print('Python version', python --version)
-print('...')
