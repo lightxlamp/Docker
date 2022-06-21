d1ebe7d66f49ac1572dfd3860efaadbc898cbe46
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-06-22 01:00:48 +0600

    feat: write backend part

---
+++ ReactApp_with_Nodejs_Postgres_and_NginX/server/index.js
@@ -0,0 +1,48 @@
+const keys = import('./keys.js');
+
+// Express APP setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // to be able to use cross-site scripting :)
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgress client setup
+const {Pool} = require("pg");
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on("connect", (client) => {
+    client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch(err => console.log('PG Error', err));
+}) 
+
+// Express route definitions 
+app.get("/", (req, res) => {
+    res.send('Hi')
+})
+
+app.get("/values/all", async(req, res) => {
+    const values = await pgClient.query("SELECT * FROM values") 
+    res.send(values)
+})
+
+app.post("/values", async(req, res) => {
+    if(!req.body.value) {
+        res.send({working: false});
+    }
+    await pgClient.query("INSTERT INTO values(number) VALUES($1)", req.body.value)
+    res.send({working: true});
+})
+
+app.listen(5000, (err) => {
+    console.log('listening');
+})
---
+++ ReactApp_with_Nodejs_Postgres_and_NginX/server/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    pgUser : process.env.PGUSER,
+    pgHost : process.env.PGHOST,
+    pgPort : process.env.PGPORT,
+    pgDataBase : process.env.PGDATABASE,
+    pgPassword : process.env.PGPASSWORD,
+}
