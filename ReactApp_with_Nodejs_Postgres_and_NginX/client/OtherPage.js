2b59f8ccf5cb98c50dd92a97078fe2bf3639a981
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-06-26 01:48:12 +0600

    feat: add packs

---
+++ ReactApp_with_Nodejs_Postgres_and_NginX/client/OtherPage.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+    return ( <
+        div >
+        Other page.Routing is working < Link to = "/" > Go back to main page < /Link>{" "} <
+        /div>
+    );
+};
+
+export default OtherPage;
