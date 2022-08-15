782da585815ff78b49cb810173d25cb065424988
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-08-16 01:53:28 +0600

    python

---
+++ UdemyCourse/python-app/main.py
@@ -0,0 +1,10 @@
+import calendar 
+
+print("Welcome to Python")
+
+year = int(input('Please enter year:'))
+month = int(input('Please enter month:'))
+
+print(calendar.month(year, month))
+
+print('Bye')
