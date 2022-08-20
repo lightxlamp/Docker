464929ec31f68e20096cacf91b775f230aefae7e
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-08-21 02:26:12 +0600

    Inspect container

---
+++ UdemyCourse/node-app/index.mjs
@@ -0,0 +1,10 @@
+import fs from "fs"
+
+fs.appendFile("my-file.txt", 'File created by Node.js', (err) => {
+    if(err) throw err;
+    console.log('File saved');
+})
+
+setTimeout(() => {
+    console.log('Time ti inspect is over');
+}, 600000);
