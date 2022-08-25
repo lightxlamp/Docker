0707df6011ff31b565ac3bf3788a2992f7c144a4
Stanislav Sheveliukhin <sheveliukhin.s@gmail.com>
2022-08-26 00:46:53 +0600

    study: try docker compose with 2 containers

---
+++ UdemyCourse/docker-compose-app/app/main.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient 
+from pprint import pprint 
+
+MONGO_URL = "mongodb://mongo:27017"
+client = MongoClient(MONGO_URL)
+db = client.admin
+db_list = db.command("listDatabases")
+pprint(db_list)
+
+pprint('Modify code -> to test image recreation')
